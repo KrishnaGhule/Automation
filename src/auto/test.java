98681b30f364cfb74b0469daaa82281d6f46375e
KrishnaGhule <116060325+KrishnaGhule@users.noreply.github.com>
2022-10-18 22:36:19 +0530

    Update test.java

--- src/auto/test.java
+++ src/auto/test.java
@@ -9,6 +9,12 @@ public class test {
 		System.out.println("testtest");
 		System.out.println("testtest");
 		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
 	}
 
 }
