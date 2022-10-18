0b4e1cd79449c781c517adf0212e925387d0985f
KrishnaGhule <116060325+KrishnaGhule@users.noreply.github.com>
2022-10-18 22:32:48 +0530

    Update test.java

--- src/auto/test.java
+++ src/auto/test.java
@@ -4,6 +4,10 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
+		System.out.println("testtest");
 	}
 
 }
